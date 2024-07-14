e9b6d1b54202ead6d2696fed70b4ac3317b861b7
aryan32134hello <12115118@nitkkr.ac.in>
2024-07-15 03:00:50 +0530

    training component added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.CNNChestClassifier import logger
 from src.CNNChestClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.CNNChestClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.CNNChestClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -19,6 +20,17 @@ STAGE_NAME = "Prepare Base Model"
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
 except Exception as e:
